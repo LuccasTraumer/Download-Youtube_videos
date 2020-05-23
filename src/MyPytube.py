6e1fa775c9c584092a7053fa42a48b8ec4cb4c55
Lucas Jesus <cc19372@g.unicamp.br>
2020-05-23 16:43:58 -0300

    metodo para fazer downloads de Playlists inclusos

--- src/MyPytube.py
+++ src/MyPytube.py
@@ -1,5 +1,8 @@
 import pytube
 import  os
+import subprocess
+
+
 caminho = 'C:/Users/Lucas/Downloads/API_DOWNLOAD'
 resolucoes = ['144p','240p','360p','480p','720p','1080p','144','240','360','480','720','1080']
 def download_video(link_video = str, resolução='360p'):
@@ -16,4 +19,20 @@ def download_video(link_video = str, resolução='360p'):
         youtube.filter(res=resolução).first().download(caminho)
         return "Download Finalizado!"
 
-    #youtube.filter(file_extension='mp4', progressive=True).order_by('resolution').first().download(caminho)
+def download_audio(link_video=str):
+    try:
+        youtube = pytube.YouTube(link_video).streams.filter(only_audio=True).first().download(caminho)
+    except:
+        return "Erro ao Baixar Audio!"
+    else:
+        return "Download finalizado com sucesso!"
+
+def download_playlist(link_playlist = str):
+    try:
+        playlist = pytube.Playlist(link_playlist)
+        print(f"Existem {len(playlist.video_urls)} videos na Playlist!")
+        playlist.download_all(caminho+"/playlist")
+    except:
+        return "Erro ao fazer Download da Playlist"
+    else:
+        return "Download Exceutado com Sucesso!"
