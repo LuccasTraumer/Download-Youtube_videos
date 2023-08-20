3b25ecb98448362ab44ceb7d683f656247b69018
Lucas Jesus <cc19372@g.unicamp.br>
2023-08-20 16:52:10 -0300

    Melhorando codigo

---
+++ src/CONSTANTES.py
@@ -0,0 +1,5 @@
+MP4 = "mp4"
+MP3 = "mp3"
+PLAYLIST = "playslist"
+
+TYPE_FILES = [MP4, MP3, PLAYLIST]
--- src/Main.py
+++ src/Main.py
@@ -1,16 +1,47 @@
 import os
+import requests
+import json
 import MyPytube
+import CONSTANTES
 
-video_ou_audio = str(input("Qual tipo de Arquivo deseja baixar? [mp3/mp4/playlist]: "))
-link_videos = str(input("URL do Audio,Video ou Playlist: "))
-print("Video será baixado na pasta Downloads/API_DOWNLOAD")
 
-if video_ou_audio.lower() == "mp4":
-    resoluçao_video = str(input("Qual Resolção Deseja? [144p/240p/360p/720p/1080p]: "))
-    print(MyPytube.download_video(link_videos, resoluçao_video))
-if video_ou_audio.lower() == "mp3":
-    print(MyPytube.download_audio(link_videos))
-if video_ou_audio.lower() == "playlist":
-    print(MyPytube.download_playlist(link_videos))
+class VideoData:
+    def __init__(self, linkVideo, typeFile):
+        self.linkVideo = linkVideo
+        self.typeFile = typeFile
+
+
+def getTypeFile():
+    videoOrAudio = str(input("Qual tipo de Arquivo deseja baixar? [mp3/mp4/playlist]: "))
+    link_videos = str(input("URL do Audio,Video ou Playlist: "))
+    pathDownload = str(input("Informe o caminho para o Download: Default será na (Downloads/API_DOWNLOAD)"))
+    return VideoData(link_videos, videoOrAudio)
+
+
+def getPlaylits():
+    print(MyPytube.download_playlist(videoData.linkVideo))
+
+def getMP3():
+    print(MyPytube.download_audio(videoData.linkVideo))
+
+def getMP4():
+    resolution = str(input("Qual Resolção Deseja? [144p/240p/360p/720p/1080p]: "))
+    print(MyPytube.download_video(videoData.linkVideo, resolution))
+
+
+videoData = getTypeFile()
+
+if videoData.typeFile in CONSTANTES.TYPE_FILES:
+    if videoData.typeFile.lower() == CONSTANTES.MP3:
+        getMP3()
+
+    elif videoData.typeFile.lower() == CONSTANTES.MP4:
+        getMP4()
+
+    else:
+        getPlaylits()
+
+else:
+    videoData = getTypeFile()
 
 
